4a8bfc4de16b52d5be24c9694310e39fd92de3cd
@ ANTU <sajolhowlader08@gmail.com>
2022-12-13 17:10:02 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <section className="App">
+      <div className="bg-indigo-700">Hello Tailwind</div>
+    </section>
+  );
+}
+
+export default App;
